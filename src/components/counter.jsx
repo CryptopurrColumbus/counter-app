06869ca21e140369ba0004f1384340c2c97a46a5
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-06-06 11:14:35 +0530

    setting attributes to tags dynamically
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 class Counter extends Component {
 	state = {
-		count: 0
+        count: 0,
+        // the url gets a 200*200 photo randomly
+        imgUrl: "https://picsum.photos/200",
+        altText: "Randomly selected picture"
 	};
 
 	render() {
 		return (
 			<React.Fragment>
+                <img src={this.state.imgUrl} alt={this.state.altText} />
 				<span>{this.formatCount()}</span>
 				<button>Increment</button>
 			</React.Fragment>
